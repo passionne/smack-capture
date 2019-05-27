28d30ef81aeef6ee051767d1267a179825e74bb1
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2019-05-27 16:41:46 +0200

    add docs install and quickstart

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from flask import Flask, send_file, send_from_directory
+from flask import Flask, send_file, send_from_directory, redirect
 from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='')
@@ -28,8 +28,12 @@ def send_node_modules(path):
     return send_from_directory('node_modules', path)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def homepage():
+    return redirect("/index.html", code=302)
+
+@app.route('/api')
+def api():
+    return "Hello World !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000 ,debug=True)
